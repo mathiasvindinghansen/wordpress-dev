2ca531c4a700540270f4041fb31f186ec7c6b0b7
mathiasvindinghansen <mvh@the-lead.dk>
2025-03-04 10:25:28 +0100

    [fix typo]

--- functions.php
+++ functions.php
@@ -4,4 +4,4 @@ function mvh_enqueue_styles() {
     wp_enqueue_style('mvh-style', __DIR__ . '/style.css');
 }
 
-add_action('wp-enqueue-scripts', 'mvh_enqueue_styles');
+add_action('wp_enqueue_scripts', 'mvh_enqueue_styles');
